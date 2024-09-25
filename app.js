bde304e790f3bf3998256106589e0b107f0c7f63
king-dato <150027784+king-dato@users.noreply.github.com>
2024-09-25 19:53:46 +0800

    Add files via upload

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 import express from "express";
 import logger from "morgan";
 import cors from "cors";
-
 import { router as contactsRouter } from "./routes/api/contactsRouter.js";
+import { router as usersRouter } from "./routes/api/usersRouter.js";
 
 const app = express();
 
@@ -13,14 +13,15 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/api/contacts", contactsRouter);
+app.use("/api/users", usersRouter);
 
-app.use((req, res) => {
+app.use((_req, res) => {
   res.status(404).json({ message: "Not found" });
 });
 
-app.use((err, req, res, next) => {
-  res.status(500).json({ message: err.message });
+app.use((err, _req, res, _next) => {
+  const { status = 500, message = "Server error" } = err;
+  res.status(status).json({ message });
 });
 
-// module.exports = app;
 export { app };
